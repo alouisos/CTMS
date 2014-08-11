f1a015b65f16f38f5c8e0b26f00f4b70adf67cba
LOUIZOS-ALEXANDROS LOUIZOS <alouisos@gmail.com>
2014-08-11 12:25:24 +0100

    favicon index.html and filter by sites added

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -1,27 +1,24 @@
 angular.module('MyApp')
   .controller('MainCtrl', ['$scope', 'Show', function($scope, Show) {
 
-    $scope.alphabet = ['0-9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-      'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-      'Y', 'Z'];
+  
 
-    $scope.genres = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy',
-      'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Food',
-      'Home and Garden', 'Horror', 'Mini-Series', 'Mystery', 'News', 'Reality',
-      'Romance', 'Sci-Fi', 'Sport', 'Suspense', 'Talk Show', 'Thriller',
-      'Travel'];
+    $scope.sites = ['Addenbrookes Hospital', 'Queen Elizabeth Hospital Birningham', 'Ipswich Hospital', 'Great Ormond Street Hospital', 'Imperial College Healthcare NHS Trust',
+      'University College London', 'Royal London Hospital', 'Oxford University Hospital', 'James Cook University Hospital', 'Queen Hospital Romford', 'Nottingham University Hospitals',
+      'Royal Sussex Hospital Brighton', 'Chapel Allerton Hospital Leeds', 'Tallaght Hospital', 'Cork University Hospital', 'Hanusch Hospital', 'Charles University Prague',
+      'University Hospital of Ludwigschafen', 'University of Luebek', 'The University Medical Center Groningen', 'University of Maastricht', 'University of Barcelona', 'Universitat Autonoma de Barcelona',
+      'University Hospital Basel', 'University of Pennsylvania', 'Cleveland Clinic Foundation', 'University of Pittsburgh', 'University of Utah', 'New York Hospital for Special Surgery', 
+      'McMaster University Hamilton', 'Mount Sinai Hospital Toronto', 'Royal Adelaide Hospital', 'Canberra Hospital', 'North Shore Hospital', 
+      'Auckland City Hospital', 'Waikato Hospital', 'Kitano Hospital', 'Chiba Hospital', 'University of Miyazaki', 'Okayama University', 'Teikyo University'];
 
-    $scope.headingTitle = 'Top 12 Shows';
+    $scope.headingTitle = 'Queries';
 
     $scope.shows = Show.query();
 
-    $scope.filterByGenre = function(genre) {
-      $scope.shows = Show.query({ genre: genre });
-      $scope.headingTitle = genre;
+    $scope.filterBySite = function(site) {
+      $scope.shows = Show.query({ site: site });
+      $scope.headingTitle = site;
     };
 
-    $scope.filterByAlphabet = function(char) {
-      $scope.shows = Show.query({ alphabet: char });
-      $scope.headingTitle = char;
-    };
+   
   }]);
